92d8ef2512bf776554554c0180aca85a502ee2e3
Dylan Brain <dylanbrain@googlemail.com>
2022-10-17 14:19:10 -0400

    contact us banner responsive

--- src/components/ContactUsBanner.js
+++ src/components/ContactUsBanner.js
@@ -1,5 +1,11 @@
+/* Component for the 'Have a project in mind?' banner. 
+At 800px the contact us button will be positioned under the text as in the design
+'Contact us' is rendered as a gatsby link so will link to the contact page when clicked
+*/
+
 import React from "react";
 import styled from "styled-components";
+import { Link } from "gatsby";
 
 export default function ContactUsBanner() {
   return (
@@ -8,7 +14,9 @@ export default function ContactUsBanner() {
         <p>Have a project in mind?</p>
       </TextDiv>
       <ContactDiv>
-        <p>Contact us</p>
+        <StyledLink to="/contact">
+          <p>Contact us</p>
+        </StyledLink>
       </ContactDiv>
     </Container>
   );
@@ -18,6 +26,12 @@ const Container = styled.div`
   height: 180px;
   background-color: #ffed00;
   display: flex;
+
+  @media screen and (max-width: 800px) {
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
 `;
 
 const TextDiv = styled.div`
@@ -28,19 +42,48 @@ const TextDiv = styled.div`
   p {
     font-size: 36px;
     font-weight: 600;
+
+    @media screen and (max-width: 1100px) {
+      font-size: 24px;
+    }
+  }
+
+  @media screen and (max-width: 800px) {
+    text-align: center;
+    margin: 20px;
   }
 `;
 
 const ContactDiv = styled.div`
   width: 20%;
   background-color: #120938;
-  display: flex;
-  align-items: center;
-  justify-content: center;
 
   p {
     color: #ffed00;
     font-size: 36px;
     font-weight: 600;
+
+    @media screen and (max-width: 1100px) {
+      font-size: 20px;
+    }
+  }
+
+  @media screen and (max-width: 800px) {
+    height: 75px;
+    width: 220px;
+    margin-bottom: 10px;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  :visited {
+    color: #ffed00;
   }
 `;
